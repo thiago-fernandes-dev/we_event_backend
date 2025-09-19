577cba85e760221de704c40cf393e4072c873dab
thiago-fernandes-dev <thiagod_paulo@hotmail.com>
2025-09-19 00:52:00 -0300

    setada a rota

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3450;
 
+app.use(express.json());
+const usuariosRoutes = require('./modules/usuarios/usuarios.routes');
+app.use('/api', usuariosRoutes);
+
 app.listen(PORT, ()=>{
     console.log(`servidor rodando em http://localhost:${PORT}`);
 });
