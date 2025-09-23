7c72ce8666869e912204a3c667b37364a8e4a834
thiago-fernandes-dev <thiagod_paulo@hotmail.com>
2025-09-23 17:15:49 -0300

    adicionado o CORS

--- src/server.js
+++ src/server.js
@@ -2,8 +2,32 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const logger = require('./modules/shared/logger/logger');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3450;
+const corsOptions = {
+    origin: function (origin, callback){
+        const recursoPermitido = ['http://localhost:5173'];
+        if(!origin) return callback(null,true);
+        if(recursoPermitido.indexOf(origin) !== -1){
+            return callback(null, true);
+        } else {
+            logger.warn(`CORS bloqueou este recurso: ${origin}`);
+            callback(new Error('Não permitido pelo CORS.'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: [
+        'Content-type',
+        'Authorization',
+        'X-Correlation-ID',
+        'Origin',
+        'Accept',
+        'autorizacao',
+    ],
+    exposedHeaders: ['X-Correlation-ID'],
+}
 
 app.use(express.json());
 const usuariosRoutes = require('./modules/usuarios/usuarios.routes');
@@ -12,6 +36,7 @@ const comprasRoutes= require('./modules/compras/compras.routes');
 app.use('/api', usuariosRoutes);
 app.use('/api/auth', usuariosAuthRoutes);
 app.use('/api/compras', comprasRoutes);
+app.use(cors(corsOptions));
 
 logger.info(
     'Start Server Application', {
